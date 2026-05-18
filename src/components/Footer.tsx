9a78f8eeec4ea893b59d921da3f8c1927570c833
sosler <shamuso23@gmail.com>
2026-05-18 13:44:40 -0500

    commented out links that don't exist

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,9 @@ export function Footer() {
       <div className="flex justify-center py-18 ">
         <div className="flex gap-4 md:max-w-5xl">
           <span className="text-paler italic font-family-lato font-bold">naked.elevator.ride@gmail.com</span>
-          <Link to="">
+          {/* <Link to="">
             <SpotifyIcon size={28} />
-          </Link>
+          </Link> */}
           <Link to="https://www.youtube.com/@NakedElevatorRide">
             <YoutubeIcon size={28} />
           </Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -36,9 +36,7 @@ function Home() {
             <div className="flex">
               <div className="flex flex-col gap-2 pb-6">
                 <span className="font-bold text-xl text-paler bg-dark p-1.5 uppercase">EP "TV Dinner", OUT MAY 28, 2026</span>
-                <div>
-                  <Button className="px-8 py-1.5">LISTEN NOW</Button>
-                </div>
+                <div>{/* <Button className="px-8 py-1.5">LISTEN NOW</Button> */}</div>
               </div>
             </div>
             <img src="https://res.cloudinary.com/shamoose/image/upload/v1779128547/NER/TV-Dinner_EP-Art_zhvjed.jpg" className="w-full max-w-5xl self-center" />
